d190ddbc30a190c0b51f6b5c74621f7c79157422
danxiaonuo <danxiaonuo@danxiaonuo.me>
2021-01-08 01:26:57 +0800

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -18,9 +18,25 @@ if [ "$VPN_SUBNET" != "" ] ; then
   echo "iptables -t nat -A POSTROUTING -s $VPN_SUBNET -j MASQUERADE"
   iptables -t nat -A POSTROUTING -s $VPN_SUBNET -j MASQUERADE
 fi
-echo "sysctl -w net.ipv4.ip_forward=1"
-sysctl -w net.ipv4.ip_forward=1
-
+sysctl -w net.ipv4.ip_forward = 1
+sysctl -w net.ipv4.conf.default.forwarding = 1
+sysctl -w net.ipv4.conf.all.forwarding = 1
+sysctl -w net.ipv6.conf.default.forwarding = 1
+sysctl -w net.ipv6.conf.all.forwarding = 1
+sysctl -w net.ipv6.conf.all.accept_redirects = 0
+sysctl -w net.ipv6.conf.all.accept_source_route = 0
+sysctl -w net.ipv6.conf.default.disable_ipv6 = 0
+sysctl -w net.ipv6.conf.all.disable_ipv6 = 0
+sysctl -w net.ipv6.conf.lo.disable_ipv6 = 0
+sysctl -w net.ipv6.conf.all.proxy_ndp = 1
+sysctl -w net.ipv6.conf.all.accept_ra = 2
+sysctl -w net.ipv6.conf.default.router_solicitations = 0
+sysctl -w net.ipv6.conf.default.accept_ra_rtr_pref = 0
+sysctl -w net.ipv6.conf.default.accept_ra_pinfo = 0
+sysctl -w net.ipv6.conf.default.accept_ra_defrtr = 0
+sysctl -w net.ipv6.conf.default.autoconf = 0  
+sysctl -w net.ipv6.conf.default.dad_transmits = 0
+sysctl -w net.ipv6.conf.default.max_addresses = 1
 rm -f /var/run*.pid
 
 echo "Running $@"
